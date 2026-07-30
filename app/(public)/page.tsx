550845a8dc40b1f64db660eca6e8933619172e38
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-07-30 14:55:27 +0600

    feat: create getMe server action to fetch current user

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,14 +1,14 @@
-// import Hero from "@/components/shared/Hero";
-// import Navbar from "@/components/shared/Navbar";
-// import { getMe } from "@/server/getMe";
+import Hero from "@/components/shared/Hero";
+import Navbar from "@/components/shared/Navbar";
+import { getMe } from "@/server/getMe";
 
-// export default async function Home() {
-//   const user = await getMe();
-//   // console.log(user?.data);
-//   return (
-//     <>
-//       <Navbar user={user} />
-//       <Hero />
-//     </>
-//   );
-// }
+export default async function Home() {
+  const user = await getMe();
+  // console.log(user?.data);
+  return (
+    <>
+      <Navbar user={user} />
+      <Hero />
+    </>
+  );
+}
